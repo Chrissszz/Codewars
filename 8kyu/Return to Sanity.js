d56a952e216d1804be1f5263147c78530d25a94b
Chrissszz <125767594+Chrissszz@users.noreply.github.com>
2023-04-09 14:11:48 -0500

    Add files via upload

---
+++ 8kyu/Return to Sanity.js
@@ -0,0 +1,17 @@
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+// function mystery() {
+//     var results = 
+//       {sanity: 'Hello'};
+//     return 
+//       results;
+//   }
+
+//Solution
+
+function mystery() {
+    var results = {
+    sanity: 'Hello'
+      };
+    return results
+  }
